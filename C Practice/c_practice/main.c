762393c8d354e29bf7ff438840188c8cdd2e0934
Sal <56096428+salvatore999uwo@users.noreply.github.com>
2024-08-29 15:10:02 -0400

    Add files via upload

---
+++ C Practice/c_practice/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+
+    v,i,j,k,l,s,a[99];
+main()
+{
+for (scanf ( "%d11 , &s) ; *a-s; v=a (j *=v] -a [i) , k=i<s, j += (v-j <S&&
+( ! k&& ! ! printf (2+ 11 \n\n%c " - ( ! l<< ! j) , 11 #Q11 (l-"v? (l "j) &1: 2] ) &&
+++ll la[i]<s&&v&&v-i+j&&v+i-j))&&! (l%=s),vl I (i==j?a[i+=k]=O:
+++a[i])>=s*k&&++a[--i]) .
+' }
+
+}
