a6c19742dde204f9efa39440fb5c369b243695f1
Sal <56096428+salvatore999uwo@users.noreply.github.com>
2024-09-05 14:27:44 -0400

    Add files via upload

---
+++ C Practice/c_practice/another_file.c
@@ -0,0 +1,21 @@
+//block of code for getting dimensional weight of a box
+/*
+int height, length, width, volume, weight;
+
+printf("Enter height: ");
+scanf("%d", &height);
+
+printf("Enter length: ");
+scanf("%d", &length);
+
+printf("Enter width: ");
+scanf("%d", &width);
+
+volume = height*length*width;
+weight = (volume+(INCHES_PER_POUND-1))/INCHES_PER_POUND;
+
+printf("Volume: %d\n", volume);
+printf("Dimensional weight: %d\n", weight);
+
+printf("guess the reciprocal of pi: %f", RECIPROCAL_OF_PI);
+*/
---
+++ C Practice/c_practice/volume_sphere_project.c
@@ -0,0 +1,37 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+//programming project 1 to find the volume of a sphere
+
+//given that radius is 10 m
+//formula is v = 3/4 pi r^3
+
+float radius = 10.0f;
+float pi = 3.14f;
+
+float volume = (4.0f/3.0f)*pi*(radius*radius*radius);
+
+printf("volume of the sphere with a radius of 10 m is %.3f, in metres cubed.", volume);
+
+return 0;
+}
+
+//this is a comment
+
+
+/* c program:
+
+int main(void)
+{
+declarations
+
+statements
+
+}
+
+
+
+*/
+
