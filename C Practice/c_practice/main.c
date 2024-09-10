fd85af2548e91857544867dd764f39d652db7127
Sal <56096428+salvatore999uwo@users.noreply.github.com>
2024-09-10 18:15:31 -0400

    Add files via upload

--- C Practice/c_practice/main.c
+++ C Practice/c_practice/main.c
@@ -1,4 +1,4 @@
-//chapter 5, selection statements
+//chapter 6, loops
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,7 +6,126 @@
 
 int main(void)
 {
+//exercise 1
+/*
+int i;
 
+i = 1;
+while(i <= 128){
+    printf("%d",i);
+    i *= 2;
+}
+
+
+//ex 2
+int i;
+i = 9384;
+
+do{
+    printf("%d\n",i);
+    i /= 10;
+} while (i > 0);
+
+//ex 3
+int i,j;
+for(i=5, j=i-1; i>0, j>0; --i, j=i-1)
+    printf("%d\n",i);
+
+
+//ex 4
+int i;
+
+for(i=0; i<3; i++){
+    printf("ha\n");
+}
+
+for(i=0; i<3; ++i){
+    printf("ho\n");
+}
+
+for(i=0; i++ < 3;){
+    printf("he\n");
+}
+
+
+//ex 5
+int i;
+i=1;
+while(i<=128){
+    printf("%d\n",i);
+    i *= 2;
+}
+
+for(i=1;i<=128;i*=2){
+    printf("%d\n",i);
+}
+
+
+//ex 7
+int i;
+i = 9384;
+
+do{
+    printf("%d\n", i);
+    i /= 10;
+} while (i > 0);
+
+
+for(i=9384; i > 0; i /= 10){
+    printf("%d\n",i);
+}
+
+
+//ex 8
+int i;
+
+for(i=10;i>=1;i /= 2){
+    printf("%d\n",i++);
+
+}
+
+
+//ex 9
+
+i=10;
+
+while(i>=1){
+    printf("%d\n",i++);
+    i /= 2;
+}
+
+blah: printf("done");
+
+
+
+//ex 10
+//replace a continue statements with equivalent goto statement
+int i;
+i=1;
+while(i<10){
+    i++;
+    printf("i is %d\n",i);
+    goto bop;
+    printf("haha");
+    bop: printf("done");
+}
+
+
+//ex 11
+int sum;
+int i;
+
+sum = 0;
+for(i=0;i<10;i++){
+    if(i % 2){
+        continue;
+    }
+    sum += i;
+}
+printf("%d\n",sum);
+*/
+
+//ex 12
 
 
 
